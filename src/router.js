83350a09192c53a6eb5a3985265de1dcc16d2c5b
Cys <chen4501260@foxmail.com>
2019-07-06 20:48:05 +0800

    究极体-路由懒加载

--- src/router.js
+++ src/router.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from './views/Login.vue'
-import Home from './views/Home.vue'
-import UserList from './views/UserList.vue'
-import RoleList from './views/RoleList.vue'
-import PermissionList from './views/PermissionList.vue'
-import GoodsList from './views/GoodsList.vue'
-import GoodsClassify from './views/GoodsClassify.vue'
-import AddGoods from './views/AddGoods.vue'
+
+// 路游懒加载
+const Login =  () => import('./views/Login.vue')
+const Home =  () => import('./views/Home.vue')
+const UserList =  () => import('./views/UserList.vue')
+const RoleList =  () => import('./views/RoleList.vue')
+const PermissionList =  () => import('./views/PermissionList.vue')
+const GoodsList =  () => import('./views/GoodsList.vue')
+const GoodsClassify =  () => import('./views/GoodsClassify.vue')
+const AddGoods =  () => import('./views/AddGoods.vue')
 
 Vue.use(Router)
 
